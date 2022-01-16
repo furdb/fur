90febc3e136e3472206d8f0764cb306121abc93e
madhavan-raja <madhavanraja99@gmail.com>
2022-01-16 14:35:52 +0530

    Set up file IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
+static DB_INFO_FILE_NAME: &str = "fur.json";
+
 fn main() {
-    println!("Hello, world!");
+    let db_path: &str = "E:\\Home\\Repositories\\fur\\TestDB";
+
+    let db_info = db_path.to_string() + &"\\".to_string() + DB_INFO_FILE_NAME;
+
+    let contents = fs::read_to_string(db_info).expect("Something went wrong reading the file");
+
+    let json: serde_json::Value =
+        serde_json::from_str(&contents).expect("JSON was not well-formatted");
+
+    println!("{0}", json);
 }

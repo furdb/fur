790deed5910a31170ce22c1a4e1f9e8233925f3c
madhavan-raja <madhavanraja99@gmail.com>
2022-01-19 21:26:57 +0530

    New DBs can now be created

--- src/fur/fur_database.rs
+++ src/fur/fur_database.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
 
@@ -6,32 +6,60 @@ pub struct FurDB {
     dir: PathBuf,
 }
 
+#[derive(Serialize, Deserialize)]
+struct FurDBInfo {
+    name: String,
+    description: String,
+}
+
+impl FurDBInfo {
+    fn new(name: String, description: String) -> FurDBInfo {
+        FurDBInfo { name, description }
+    }
+}
+
 impl FurDB {
     pub fn new(dir: PathBuf) -> FurDB {
-        FurDB { dir: dir }
+        FurDB { dir }
     }
 
-    pub fn get_name(&self) -> Result<String, Box<dyn Error>> {
+    pub fn create(&self, name: String, description: String) -> std::io::Result<()> {
+        if !self.dir.exists() {
+            std::fs::create_dir(&self.dir)?;
+        }
+
+        let db_info_file_path = self.get_info_file_path();
+
+        let db_info = FurDBInfo::new(name, description);
+
+        let db_info_contents = serde_json::to_string(&db_info)?;
+
+        fs::write(db_info_file_path, db_info_contents)?;
+
+        Ok(())
+    }
+
+    pub fn get_name(&self) -> std::io::Result<String> {
         self.get_info("name")
     }
 
-    pub fn get_description(&self) -> Result<String, Box<dyn Error>> {
+    pub fn get_description(&self) -> std::io::Result<String> {
         self.get_info("description")
     }
 
-    pub fn get_tables(&self) -> Vec<String> {
+    pub fn get_tables(&self) -> std::io::Result<Vec<String>> {
         let mut tables = Vec::new();
 
-        for file in fs::read_dir(&self.dir).unwrap() {
-            let file_name = file.unwrap();
+        for file in fs::read_dir(&self.dir)? {
+            let file_name = file?;
             let metadata = fs::metadata(&file_name.path());
 
-            if metadata.unwrap().is_dir() {
+            if metadata?.is_dir() {
                 tables.push(file_name.file_name().to_string_lossy().to_string());
             }
         }
 
-        tables
+        Ok(tables)
     }
 
     fn get_info_file_path(&self) -> PathBuf {
@@ -41,14 +69,14 @@ impl FurDB {
         db_info_file_path
     }
 
-    fn get_info(&self, property: &str) -> Result<String, Box<dyn Error>> {
+    fn get_info(&self, property: &str) -> std::io::Result<String> {
         let db_info_file_path = self.get_info_file_path();
 
         let db_info_contents_raw = fs::read_to_string(&db_info_file_path)?;
 
         let db_info_contents: serde_json::Value = serde_json::from_str(&db_info_contents_raw)?;
 
-        let value = db_info_contents.get(property).unwrap().to_string();
+        let value = String::from(db_info_contents.get(property).unwrap().as_str().unwrap());
 
         Ok(value)
     }
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::path::PathBuf;
 mod fur;
 use fur::FurDB;
 
-// mod cli;
-// use clap::Parser;
+fn main() -> std::io::Result<()> {
+    // Set the path to the DB
+    let db_path = PathBuf::from("E:\\Home\\Repositories\\fur\\TestDBs\\TestDB2");
 
-fn main() {
-    // let _args = cli::Args::parse();
-
-    // Path to the DB
-    let db_path = PathBuf::from("E:\\Home\\Repositories\\fur\\TestDB");
-
-    // DB object
+    // Create the DB object
     let db = FurDB::new(db_path);
 
-    // Get DB info
+    // Create the DB structure
+    // Note: The DB should NOT already exist. Overwrites the DB info if the info file exists
+    db.create(
+        String::from("Another DB of mine!"),
+        String::from("Another sample description"),
+    )?;
+
+    // Get the DB info
+    // Note: DB should already exist
     println!("Name: {0}", db.get_name().unwrap());
     println!("Description: {0}", db.get_description().unwrap());
-    println!("Tables: {:?}", db.get_tables());
+    println!("Tables: {:?}", db.get_tables().unwrap());
+
+    Ok(())
 }
